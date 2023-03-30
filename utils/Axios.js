ff690ee399b82fb0abd8519ea1eea4a943a96991
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-03-30 13:56:38 +0600

    url change for pro

--- utils/Axios.js
+++ utils/Axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const baseURL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://blog-app-mhs.vercel.app/api"
-//     : "http://localhost:3000/api";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://blog-app-mhs.vercel.app/api"
+    : "http://localhost:3000/api";
 
 const instance = axios.create({
-  baseURL: "https://blog-app-mhs.vercel.app/api",
+  baseURL,
 });
 
 export default instance;
